e7c4ea7a67138fe563c06982423cbf3110b6cc48
David <david-gelmut@rambler.ru>
2026-07-01 15:22:56 +0500

    FIX: Правки

--- frontend/src/js/bootstrap.js
+++ frontend/src/js/bootstrap.js
@@ -6,8 +6,8 @@ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-axios.defaults.baseURL = 'http://localhost:8000';
-
+//axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 axios.interceptors.response.use(
     (response) => response,
     (error) => {
