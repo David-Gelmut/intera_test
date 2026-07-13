967c9b1eb1b01bdbbac432738bbbec2955c00c6e
David <david-gelmut@rambler.ru>
2026-07-13 15:47:25 +0500

    FIX: Правки.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -12,9 +12,6 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
         Event::forget(Registered::class, SendEmailVerificationNotification::class);
     }
 }
