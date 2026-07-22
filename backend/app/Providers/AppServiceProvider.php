d866b4f06caef5699433a5d302b7f4312024884b
David <david-gelmut@rambler.ru>
2026-07-22 20:31:32 +0500

    FIX: Правки чата.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -12,6 +12,10 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Event::forget(Registered::class, SendEmailVerificationNotification::class);
     }
 }
